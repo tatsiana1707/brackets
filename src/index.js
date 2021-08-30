8360263bc9cd306d893aa7041adba852757b5404
Tatsiana1707 <tannysha1987@mail.ru>
2021-08-30 22:47:35 +0300

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,42 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    stack = [],
+    o = bracketsConfig.reduce(function(prev,curr){prev[curr[0]]=curr[1];return prev;},{})
+    let open = Object.keys(o);
+    let close = Object.values(o);
+  
+     console.log(o);
+    console.log(open);
+    console.log(close);
+var chars = str.split('');
+if (str.length % 2 == 1) {
+    return false;
+}
+for (var i = 0, len = chars.length; i < len; i++) {
+ if(chars[i] === chars[chars.length -1] && chars.length % 2 === 0){
+   return true;
+ }
+  let openIndex = open.indexOf(chars[i]);
+   if (openIndex !== -1) {
+       
+       stack.push(openIndex);
+       continue;
+       
+   }
+
+   let closeIndex = close.indexOf(chars[i]);
+   if (closeIndex !== -1) {
+       
+       openIndex = stack.pop();
+       if (closeIndex !== openIndex) {
+           return false;
+       }
+   }
+
+}
+
+if (stack.length !== 0) {
+    return false;
+}
+
+return true;
 }
